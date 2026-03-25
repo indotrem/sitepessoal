fe3d54c3a2eecc59313f8c6b24499014346f5ec5
Ícaro Coelho <icarocoelhosilva2006@gmail.com>
2026-03-25 04:04:17 -0300

    iniciado portifolio e mudando sobre

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ toggle.addEventListener("change", function () {
     document.body.classList.toggle("light-mode");
     
 });
+
+function abrirLightbox() {
+  const lightbox = document.getElementById('lightbox');
+  const vSmall = document.getElementById('video-small');
+  const vFull = document.getElementById('video-full');
+  lightbox.classList.add('ativo');
+  vFull.currentTime = vSmall.currentTime;
+  vFull.play();
+}
+
+function fecharVideo() {
+  const lightbox = document.getElementById('lightbox');
+  const vSmall = document.getElementById('video-small');
+  const vFull = document.getElementById('video-full');
+  lightbox.classList.remove('ativo');
+  vSmall.currentTime = vFull.currentTime;
+  vFull.pause();
+}
